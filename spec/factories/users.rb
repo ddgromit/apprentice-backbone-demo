462578423b6c558641e09e8ad7d23cac6e1395bc
Derek Dahmer <derekdahmer@gmail.com>
2013-06-14 15:17:30 -0400

    Create a user model and view

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    score { rand(1..100) }
+  end
+end
